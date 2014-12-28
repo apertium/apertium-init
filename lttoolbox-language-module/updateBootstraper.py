6f3484f3bd44d1f912c2dd9d3c7aea61e133718e
Sushain Cherivirala <sushain@skc.name>
2014-12-28 16:30:38 -0600

    Move and generalize apertium-init

--- lttoolbox-language-module/updateBootstraper.py
+++ lttoolbox-language-module/updateBootstraper.py
@@ -7,27 +7,27 @@ files = [
     'apertium-{{languageCode}}.{{languageCode}}.acx',
     'apertium-{{languageCode}}.{{languageCode}}.dix',
     'apertium-{{languageCode}}.{{languageCode}}.rlx',
-    'apertium-{{languageCode}}.pc.in', 
+    'apertium-{{languageCode}}.pc.in',
     'AUTHORS',
     'autogen.sh',
-    'configure.ac', 
+    'configure.ac',
     'COPYING',
     'Makefile.am',
-    'modes.xml', 
+    'modes.xml',
     'README'
 ]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Update the bootstraper script for an Apertium lttoolbox-based language module')
     parser.add_argument('-d', '--vanillaDirectory', help='Location of directory with vanilla files', default=os.getcwd())
-    parser.add_argument('-f', '--bootstraperScript', help='Location of bootstraper script', default='./bootstrap.py')
+    parser.add_argument('-f', '--bootstraperScript', help='Location of bootstraper script', default='../apertium-init.py')
     args = parser.parse_args()
-    
+
     encodedFiles = {}
-    
+
     for filename in files:
         with open(os.path.join(args.vanillaDirectory, filename), 'rb') as f:
             encodedFiles[filename] = base64.b64encode(f.read())
-    
+
     for line in fileinput.input([args.bootstraperScript], inplace=True):
-        sys.stdout.write(re.sub(r'^files = {.*?}$', 'files = %s' % repr(encodedFiles), line))
+        sys.stdout.write(re.sub(r'^lttoolbox_language_module_files = {.*?}$', 'lttoolbox_language_module_files = %s' % repr(encodedFiles), line))
