7682a9bca1403f680f1989367ac29d5503dd4d5b
Sushain Cherivirala <sushain@skc.name>
2014-12-30 18:35:38 -0600

    Changes to help for bootstrap updaters

--- hfst-language-module/updateBootstraper.py
+++ hfst-language-module/updateBootstraper.py
@@ -19,9 +19,9 @@ files = [
 ]
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Update the bootstraper script for an Apertium lttoolbox-based language module')
-    parser.add_argument('-d', '--vanillaDirectory', help='Location of directory with vanilla files', default=os.getcwd())
-    parser.add_argument('-f', '--bootstraperScript', help='Location of bootstraper script', default='../apertium-init.py')
+    parser = argparse.ArgumentParser(description='Update the bootstraper script for an Apertium HFST-based language module')
+    parser.add_argument('-d', '--vanillaDirectory', help='location of directory with vanilla files', default=os.getcwd())
+    parser.add_argument('-f', '--bootstraperScript', help='location of bootstraper script', default='../apertium-init.py')
     args = parser.parse_args()
 
     encodedFiles = {}
--- lttoolbox-language-module/updateBootstraper.py
+++ lttoolbox-language-module/updateBootstraper.py
@@ -21,8 +21,8 @@ files = [
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Update the bootstraper script for an Apertium lttoolbox-based language module')
-    parser.add_argument('-d', '--vanillaDirectory', help='Location of directory with vanilla files', default=os.getcwd())
-    parser.add_argument('-f', '--bootstraperScript', help='Location of bootstraper script', default='../apertium-init.py')
+    parser.add_argument('-d', '--vanillaDirectory', help='location of directory with vanilla files', default=os.getcwd())
+    parser.add_argument('-f', '--bootstraperScript', help='location of bootstraper script', default='../apertium-init.py')
     args = parser.parse_args()
 
     encodedFiles = {}
