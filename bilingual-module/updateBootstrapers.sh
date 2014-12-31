c2d76006c03f108673335feaa0649b60caaed2ed
Sushain Cherivirala <sushain@skc.name>
2014-12-30 19:30:13 -0600

    Add script to update 3 bootstrapers

---
+++ bilingual-module/updateBootstrapers.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+cd hfst-bilingual-module/
+./updateBootstraper.py
+cd ../lttoolbox-bilingual-module/
+./updateBootstraper.py
+cd ../hfst-lttoolbox-bilingual-module/
+./updateBootstraper.py
+
