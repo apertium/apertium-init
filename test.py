ed1656b7304fafe29689b412692e8539d229ddd1
mr-martian <popcorn.tomato.dude@gmail.com>
2019-05-12 12:17:59 -0400

    XML headers, lrx-proc args, and tests (#65)
    * xml headers, tweaks, and tests
    
    * Update test.py
    
    Co-Authored-By: Sushain Cherivirala <sushain@skc.name>

--- test.py
+++ test.py
@@ -55,6 +55,17 @@ class TestInvalidModule(unittest.TestCase):
         with self.assertRaises(SystemExit):
             apertium_init.main(['eng', '--no-rlx1', '--no-prob1'])
 
+    def test_invalid_analyser(self):
+        with self.assertRaises(SystemExit):
+            apertium_init.main(['eng', '--analyser=bloop'])
+
+    def test_dir_already_exists(self):
+        path = make_path('eng')
+        os.makedirs(path)
+        with self.assertRaises(SystemExit):
+            apertium_init.main(['eng'])
+        shutil.rmtree(path)
+
 
 class TestLttoolboxModule(TestModule, unittest.TestCase):
     @classmethod
@@ -74,22 +85,41 @@ class TestTwocModule(TestModule, unittest.TestCase):
         apertium_init.main([cls.name, '--analyser=hfst', '--with-twoc'])
 
 
-class TestLttoolboxPair(TestModule, unittest.TestCase):
+class TestUnknownCodeModule(TestModule, unittest.TestCase):
+    name = 'bkl'
+    path = make_path(name)
+
+    @classmethod
+    def setUpClass(cls):
+        apertium_init.main([cls.name])
+
+
+class TestPair(TestModule, unittest.TestCase):
     name = 'eng-cat'
     path = make_path(name)
 
     name1 = 'eng'
     path1 = make_path(name1)
+    analyser1 = ''
 
     name2 = 'cat'
     path2 = make_path(name2)
+    analyser2 = ''
 
     @classmethod
     def setUpClass(cls):
-        for name, path in [(cls.name1, cls.path1), (cls.name2, cls.path2)]:
-            apertium_init.main([name])
+        for name, path, analyser in [(cls.name1, cls.path1, cls.analyser1), (cls.name2, cls.path2, cls.analyser2)]:
+            if analyser:
+                apertium_init.main([name, '--analyser={}'.format(analyser)])
+            else:
+                apertium_init.main([name])
             build(path)
-        apertium_init.main([cls.name])
+        pair_args = [cls.name]
+        if cls.analyser1:
+            pair_args.append('--analyser1={}'.format(cls.analyser1))
+        if cls.analyser2:
+            pair_args.append('--analyser2={}'.format(cls.analyser2))
+        apertium_init.main(pair_args)
 
     @classmethod
     def tearDownClass(cls):
@@ -99,6 +129,30 @@ class TestLttoolboxPair(TestModule, unittest.TestCase):
     def test_builds(self):
         autogen_args = ['--with-lang1=../{}'.format(self.path1), '--with-lang2=../{}'.format(self.path2)]
         build(self.path, autogen_args=autogen_args)
+
+
+class TestDefaultPair(TestPair, unittest.TestCase):
+    pass
+
+
+class TestLttoolboxPair(TestPair, unittest.TestCase):
+    analyser1 = 'lt'
+    analyser2 = 'lttoolbox'
+
+
+class TestHfstPair(TestPair, unittest.TestCase):
+    analyser1 = 'hfst'
+    analyser2 = 'hfst'
+
+
+class TestHfstLtPair(TestPair, unittest.TestCase):
+    analyser1 = 'hfst'
+    analyser2 = 'lttoolbox'
+
+
+class TestLtHfstPair(TestPair, unittest.TestCase):
+    analyser1 = 'lt'
+    analyser2 = 'hfst'
 
 
 if __name__ == '__main__':
