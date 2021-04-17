4f197bf968d4a5e34b2787a6673620d778a85d73
Sushain Cherivirala <sushain@skc.name>
2021-04-16 21:41:03 -0700

    Drop support for Py 3.5

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
